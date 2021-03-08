624475d55a6c1ed5d2da8a61085ed14bf7428f1a
unconsciou5 <svensteinert96@gmail.com>
2021-03-08 01:09:44 +0100

    Rocket Engine introduction

--- engine.py
+++ engine.py
@@ -1,18 +1,23 @@
 import numpy as np
 import time
 
-dt = 0.01 # timestep size in seconds
-g = 9.81 # gravitational acceleration
-a = np.array([0,0,-g]) # acceleration vector
-v = np.array([0,0,0]) # velocity vector
-x = np.array([0,0,100]) # location vector
+dt = 0.01                       # timestep size in seconds
+a_g = np.array([0,0,-9.81])     # gravitational acceleration
+a_re_flat = 14                # rocket engine flat acceleration
+a_re_dir = np.array([0,0,1])    # rocket engine acceleration direction -> normed vector
+a_re = a_re_flat*a_re_dir       # rocket engine acceleration
+# INITIALS
+a = np.array([0,0,0])           # acceleration vector
+v = np.array([0,2,-10])           # velocity vector
+x = np.array([0,0,10])          # location vector
 
-v_old = np.array([0,0,0]) # velocity vector from previous timestep
+v_old = v                       # velocity vector from previous timestep
 
 print('┌────────────────────────┐')
 print('│        SETTINGS        │')
 print('├────────────────────────┤')
-print('│  g │ ' + str(g) + ' m/s²         │')
+print('│  g │ ' + str(a_g[2]) + ' m/s²        │')
+print('│ re │ ' + str(a_re_flat) + ' m/s²         │')
 print('│ dt │ ' + str(dt) + ' s            │')
 print('└────────────────────────┘')
 print('Initial values')
@@ -26,8 +31,10 @@ print(' s                           x                                 v         
 print("{:.3f}".format(0) + ' │          │ ' + "{:.3f}".format(x[0]) + ' │ ' + "{:.3f}".format(x[1]) + ' │ ' + "{:.3f}".format(x[2]) + ' │         │ ' + "{:.3f}".format(v[0]) + ' │ ' + "{:.3f}".format(v[1]) + ' │ ' + "{:.3f}".format(v[2]) + ' │        │ ' + "{:.3f}".format(a[0]) + ' │ ' + "{:.3f}".format(a[1]) + ' │ ' + "{:.3f}".format(a[2])+ ' │ ')
 f = open("log.csv", "w")
 for t_steps in range(1,2000):
-    v = v_old + a*dt
-    x = x + v_old*dt + 0.5*(v - v_old)*dt
+    a = a_g + a_re                              # compute new acceleration
+    v = v_old + a*dt                            # compute new velocity
+    x = x + v_old*dt + 0.5*(v - v_old)*dt       # compute new location
+
     v_old = v
     t = t_steps * dt
 
